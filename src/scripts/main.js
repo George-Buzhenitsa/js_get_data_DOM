52d868cdd34d47c7ac5cb8b469829ec79c5526d1
George <george.boujhenitsa@gmail.com>
2025-02-25 17:24:30 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+const getPopulationAmount = Array.from(population)
+  .map((n) => n.innerHTML.split(',').join(''))
+  .map((n) => Number(n));
+
+const getTotal = getPopulationAmount.reduce((total, n) => total + n, 0);
+const getAverage = Math.floor(getTotal / 9);
+
+totalPopulation.textContent = transformAmount(getTotal);
+averagePopulation.textContent = transformAmount(getAverage);
+
+function transformAmount(value) {
+  const valueToString = String(value);
+  const startIndex = valueToString.length % 3 === 0 ? 0 : 1;
+  const tempArray = [];
+  let tempStr = '';
+
+  if (startIndex === 1) {
+    tempArray.push(valueToString[0]);
+  }
+
+  for (let i = startIndex; i <= valueToString.length; i++) {
+    if (tempStr.length === 3) {
+      tempArray.push(tempStr);
+      tempStr = '';
+    }
+
+    tempStr += valueToString[i];
+  }
+
+  return tempArray.join(',');
+}
